4c4fb21c5bbb2f7da99ceb95d2b2bcbc11f5796e
jonyweb20 <jonyweb20@gmail.com>
2022-04-11 00:45:55 +0700

    v.1.2
    CardBuy

--- card.js
+++ card.js
@@ -1,47 +1,46 @@
 let readme = document.querySelectorAll('.cardInfoIcon');
-//console.log(readme[0].className);
 for (let i = 0; i < readme.length; i++) {
-    readme[i].addEventListener('click', selectIcon);
+  readme[i].addEventListener('click', selectIcon);
 }
 
 function selectIcon() {
-    let itemClass = this.parentNode.className;
-    let itemNode = this.parentNode;
-    console.log(itemNode);
-    if (itemClass === 'cardInfo-Readme') {
-        dropDownReadme(itemNode);
-    } else {
-        dropDownBuy(itemNode);
-    }
+  let itemClass = this.parentNode.className;
+  let itemNode = this.parentNode;
+  if (itemClass === 'cardInfo-Readme') {
+    dropDownReadme(itemNode);
+  } else {
+    dropDownBuy(itemNode);
+  }
 }
 
 function dropDownReadme(item) {
-    let descrip = item.querySelector('#DropdownReadme');
-    let selectDrop = descrip.classList.toggle("readme-description"); 
-    console.log(descrip);
-    console.log(selectDrop);
+  let descrip = item.querySelector('#DropdownReadme');
+  let readmeOut = descrip.classList.toggle("readme-description");
 }
-window.onclick = function(event) {
+
+function dropDownBuy(item) {
+  let descrip = item.querySelector('#DropdownBuy');
+  let readmeOut = descrip.classList.toggle("readme-description");
+  console.log(item);
+}
+
+/*   if(!descrip.classList.contains('readme-description')){
+    descrip.classList.toggle("readmeActiv");
+    console.log(descrip);
+  } */
+
+/* window.onclick = function (event) {
   if (!event.target.matches('cardInfo-Readme')) {
     let dropdowns = document.getElementById("DropdownReadme");
-       console.log(dropdowns);
+    console.log(dropdowns);
     for (let i = 0; i < dropdowns.length; i++) {
       if (dropdowns[i].classList.contains('readme-description')) {
+
+
         dropdowns[i].classList.remove('readme-description');
       }
     }
   }
-}
+}  */
 
 
-
-
-
-
-/*else  {item.classList.remove ('readme-DescriptionFunction')};*/
-
-
-//console.log(item);
-function dropDownBuy(item) {
-    console.log(item);
-}
