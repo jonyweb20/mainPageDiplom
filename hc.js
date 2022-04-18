6c996819be21f82f805b387c55f012b0bfcc5057
jonyweb20 <jonyweb20@gmail.com>
2022-04-19 00:24:25 +0700

    v.1.3
    главная страница (home)+js+доп шрифты

---
+++ hc.js
@@ -0,0 +1,54 @@
+let textIntro = document.querySelectorAll('.intro');
+//определяем время задержки
+document.addEventListener("DOMContentLoaded", function () {
+    let j = n = 0;
+    let text = textIntro[j].innerHTML;
+    textInSpan(textIntro[j], n);
+    setTimeout(function () {
+        let text = textIntro[j++].innerHTML;
+        textInSpan(textIntro[j]);
+       
+    }, 7000);
+    setTimeout(function () {
+        let text = textIntro[j++].innerHTML;
+        textInSpan(textIntro[j],n);
+        
+    }, 15000);
+    
+    setTimeout(function () {
+        let text = textIntro[j++].innerHTML;
+        textInSpan(textIntro[j],n);
+        
+    }, 22000);
+})
+
+function textInSpan(text, nb) {
+    //debugger;
+    let arrayWord = Array.from(text.innerHTML);
+    //console.log(arrayWord);
+    for(let i=0; i<arrayWord.length; i++){
+        let rel = document.querySelector('.AboutInfo-rel');
+        let span = document.createElement('span');
+        let textNode = document.createTextNode(arrayWord[i]);
+       // console.log(textNode);
+          //на пробелы
+        if (textNode.textContent === " ")
+    {
+        let nv = span.textContent = "&#032;";
+       // console.log(nv);
+
+    }
+        if(even_or_odd(i)==="even"){
+            span.setAttribute('class', 'even');   
+        }
+        //добавление символа
+
+        span.innerHTML = textNode.textContent;
+        rel.append(span);
+
+    }
+}
+//проверка на четность
+function even_or_odd(number) {
+    return number % 2 === 0 ? "Odd" : "even"
+}
---
+++ nav.js
@@ -0,0 +1,47 @@
+//let phoneBlock = document.querySelector('.breadcrumb-phone');
+let labPhone = document.querySelector('.labelPhone');
+let phoneListNumber = document.querySelector('.phoneNumber');
+
+//Эффект телефона
+
+labPhone.addEventListener('click', function (){
+        phoneListNumber.classList.toggle("phoneBlock");
+        console.log(phoneListNumber);
+      /*labPhone.classList.add('labPhone');
+      phoneBlock.classList.add('phoneBlock');
+      phoneListNumber.classList.add('phoneListNumber');*/
+  })
+
+//Стрелки навигации
+let moveLeft = document.querySelector('.moveLeft');
+let arrowRight = document.querySelector('.arrowRight');
+let arrowCenter = document.querySelector('.arrowCenter');
+  let start = Date.now();
+let timer = setInterval(function(){
+   let timebebin = Date.now() -  start;
+    if(timebebin > 3000){
+        clearInterval(timer);
+        return
+    }
+    draw(timebebin);
+},2000);
+function draw(timebebin){
+    let title = document.getElementsByTagName('title');
+    //let blocktitle = d
+   
+    moveLeft.style.transform = 'translate('+ 200 + 'px, 0)';
+    arrowRight.style.transform = 'translate('+ -200 + 'px, 0)';
+    if(title[0].innerHTML ==="Index"){
+        arrowCenter.style.opacity = 1; 
+    }
+}
+
+//Выпадающее список из меню-гамбургера
+ let dm =document.querySelector ('.navbar-menuIcon');
+ dm.addEventListener('click', dropdownMenu);
+function dropdownMenu(){
+    let menu = document.querySelector('.navbar-collapse');
+    menu.classList.toggle("dropMenuShow");
+    console.log(menu);
+}
+    
